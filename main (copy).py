4be7a33556dd512196a4793f6c946d1b8be0c3d6
Parth-Airan <132052702+Parth-Airan@users.noreply.github.com>
2023-05-12 11:16:25 +0530

    CALC
    Calc for python run command

---
+++ main (copy).py
@@ -0,0 +1,20 @@
+a = input("Enter  your number: ")
+c = input("Enter your operator: ")
+b = input("Enter your anoter number: ")
+result = int(a) + int(b)
+
+a = int(a)
+b = int(b)
+
+if c == "+":
+  print(a+b)
+
+elif c == "-":
+  print(a-b)
+
+elif c == "*":
+ print(a*b)
+
+elif c == "/":
+  print(a/b)
+
